9a86c181847cafb0158cf3f1f905cbd694fa5676
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-09 00:46:35 +0900

    로그인 기능 구현(리팩토링 전)  및 useInput 커스텀 훅 생성

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,46 @@
 /* eslint-disable */
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import useInput from "../hooks/useInput";
 
 const LogIn = () => {
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const [signInError, setSignInError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      axios
+        .post("https://www.pre-onboarding-selection-task.shop/auth/signin", {
+          email,
+          password,
+        })
+        .then((response) => {
+          const token = response.data.accessToken;
+          localStorage.setItem("token", token);
+          navigate("/todo");
+        })
+        .catch((error) => {
+          console.log(error);
+          setSignInError(error.response.data.message);
+        });
+    },
+    [email, password]
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/todo");
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+      <form className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
         <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>
         <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
           <div className="px-5 py-7">
@@ -14,6 +49,9 @@ const LogIn = () => {
             </label>
             <input
               type="email"
+              name="email"
+              onChange={onChangeEmail}
+              value={email}
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
               data-testid="email-input"
               placeholder="write your Email"
@@ -23,12 +61,17 @@ const LogIn = () => {
             </label>
             <input
               type="password"
+              name="password"
+              onChange={onChangePassword}
+              value={password}
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
               data-testid="password-input"
               placeholder="***********"
             />
+            {signInError && <div className="mb-3">{signInError}</div>}
             <button
-              type="button"
+              type="submit"
+              onClick={handleLogin}
               className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
               data-testid="signin-button"
             >
@@ -117,7 +160,7 @@ const LogIn = () => {
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };

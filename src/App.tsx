b88f79388c41bf3605b8600c82da05936fc240ab
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-12 15:35:47 +0900

    Header 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React, { useEffect } from "react";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import LogIn from "./pages/login";
 import SignUp from "./pages/signup";
 import TodoList from "./pages/todoList";
 import "./index.css";
+import Main from "./pages/main";
+import Header from "./components/Header/header";
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    console.log(token);
-    if (token) {
-      navigate("/todo");
-    } else {
-      navigate("/signin");
-    }
-  }, []);
-
   return (
     <div>
+      <Header />
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<LogIn />} />
         <Route path="/todo" element={<TodoList />} />
